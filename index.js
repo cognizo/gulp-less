c9650b52e18f1647e5c8fba9da03e3fad27f4fe5
Graham Floyd <gfloyd@catsone.com>
2014-01-11 13:55:24 -0600

    Use event-stream map() instead of through.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var through = require('through');
 var less = require('less');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var path = require('path');
 var defaults = require('lodash.defaults');
+var es = require('event-stream');
 
 module.exports = function (options) {
 
-  function parseLess (file) {
+  function parseLess (file, callback) {
     var self = this;
     if (file.isNull()) return this.queue(file); // pass along
-    if (file.isStream()) return self.emit('error', new PluginError('gulp-less', 'Streaming not supported'));
+    if (file.isStream()) return callback(new PluginError('gulp-less', 'Streaming not supported'));
 
     // set the default options
     var opts = defaults(options || {}, {
@@ -20,22 +20,17 @@ module.exports = function (options) {
 
     // let people use their own compressor
     delete opts.compress;
-    this.pause();
 
     var str = file.contents.toString('utf8');
     less.render(str, opts, function (err, css) {
-      if (err) {
-        self.emit('error', new PluginError('gulp-less', err));
-        return self.resume();
-      }
+      if (err) return callback(new PluginError('gulp-less', err));
+
       file.contents = new Buffer(css);
       file.path = gutil.replaceExtension(file.path, '.css');
-      self.emit('data', file);
-      self.resume();
+
+      callback(null, file);
     });
   }
 
-  return through(parseLess);
+  return es.map(parseLess);
 };
-
-
